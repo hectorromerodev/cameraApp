3eb66747691a8f097250d198280f778423ff2f21
hectorromerodev <hectorromero.developer@gmail.com>
2020-07-10 19:45:14 -0700

    ADD Camera UI and controller

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { Camera, CameraResultType, CameraSource, Capacitor } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  @ViewChild('filePicker', { static: false }) filePickerRef: ElementRef<HTMLInputElement>;
+  photo: SafeResourceUrl;
+  isDesktop: boolean;
+
+  constructor(
+    private platform: Platform,
+    private sanitizer: DomSanitizer
+  ) { }
+
+  ngOnInit() {
+    if ((this.platform.is('mobile') && this.platform.is('hybrid')) || this.platform.is('desktop')) {
+      this.isDesktop = true;
+    }
+  }
+
+  async getPicture(type: string) {
+    if (!Capacitor.isPluginAvailable('Camera') || (this.isDesktop && type === 'gallery')) {
+      this.filePickerRef.nativeElement.click();
+      return;
+    }
+
+    const image = await Camera.getPhoto({
+      quality: 100,
+      width: 400,
+      allowEditing: true,
+      resultType: CameraResultType.DataUrl,
+      source: CameraSource.Prompt
+    });
+    this.photo = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+  }
+
+  onFileChoose(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    const pattern = /image-*/;
+    const reader = new FileReader();
+
+    if (!file.type.match(pattern)) {
+      console.log('File format not supported');
+      return;
+    }
+
+    reader.onload = () => {
+      this.photo = reader.result.toString();
+    }
+    reader.readAsDataURL(file);
+  }
 
 }
